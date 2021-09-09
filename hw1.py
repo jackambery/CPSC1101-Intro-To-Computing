642ee4e2c4b646ad67545f0d8622ab2bb70e3305
Jack Ambery <jack.ambery@gmail.com>
2021-09-09 16:42:07 -0400

    more hw1 progress

--- hw1.py
+++ hw1.py
@@ -9,14 +9,15 @@
 from math import *
 
 print ("Zero is", 4 + 4 - 4 - 4)
-print ("One is", sqrt(pow(4, 4)) - 4)
+print ("One is", (4 -4) + (4 / 4))
 print ("Two is", sqrt(4 * 4 * 4) / 4)
 print ("Three is", (4 + 4 + 4) / 4)
 print ("Four is", sqrt(4 * 4 * 4) / sqrt(4))
-print ("Five is", 4+4-4-4)
+print ("Five is", (4 / 4) + (sqrt(4) + sqrt(4)))
 print ("Six is", 4+4-4-4)
 print ("Seven is", 4+4-4-4)
 print ("Eight is", sqrt(4) + sqrt(4) + sqrt(4) + sqrt(4))
 print ("Nine is", )
 print ("Eleven is", 44 / (sqrt(4) + sqrt(4)))
+print ("Twelve is", sqrt(pow(4, 4)) - (sqrt(4) + sqrt(4)))
 print ("Fourteen is", 4 * 4 - (4 - sqrt(4)))
