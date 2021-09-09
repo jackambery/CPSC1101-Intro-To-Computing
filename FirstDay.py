0c35fff0f813f05db8eaa271aef5348a8f519a43
Jack Ambery <jack.ambery@gmail.com>
2021-09-09 16:25:59 -0400

    Short in class program and good start on hw1

---
+++ FirstDay.py
@@ -0,0 +1,9 @@
+# First Day In-Class
+# Filename:  FirstDay.py
+# Name: John (Jack) Ambery
+
+from math import *
+
+print ("Zero is", 4+4-4-4)
+
+print("dont name your dog shark if you live at the beach")
---
+++ hw1.py
@@ -0,0 +1,22 @@
+# hw1
+
+# Filename: hw1.py
+#
+#
+# Name: John (Jack) Ambery
+#
+
+from math import *
+
+print ("Zero is", 4 + 4 - 4 - 4)
+print ("One is", sqrt(pow(4, 4)) - 4)
+print ("Two is", sqrt(4 * 4 * 4) / 4)
+print ("Three is", (4 + 4 + 4) / 4)
+print ("Four is", sqrt(4 * 4 * 4) / sqrt(4))
+print ("Five is", 4+4-4-4)
+print ("Six is", 4+4-4-4)
+print ("Seven is", 4+4-4-4)
+print ("Eight is", sqrt(4) + sqrt(4) + sqrt(4) + sqrt(4))
+print ("Nine is", )
+print ("Eleven is", 44 / (sqrt(4) + sqrt(4)))
+print ("Fourteen is", 4 * 4 - (4 - sqrt(4)))
