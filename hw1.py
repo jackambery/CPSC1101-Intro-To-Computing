d1d9e179e8628392e94f199af2e1de2c74f3a963
Jack Ambery <jack.ambery@gmail.com>
2021-09-09 17:14:00 -0400

    complete

--- hw1.py
+++ hw1.py
@@ -4,20 +4,23 @@
 #
 #
 # Name: John (Jack) Ambery
-#
+# Using four 4's and any operation to compute 11 of the vaules from 0 to 14
 
 from math import *
 
 print ("Zero is", 4 + 4 - 4 - 4)
-print ("One is", (4 -4) + (4 / 4))
+print ("One is", (4 - 4) + (4 / 4))
 print ("Two is", sqrt(4 * 4 * 4) / 4)
 print ("Three is", (4 + 4 + 4) / 4)
 print ("Four is", sqrt(4 * 4 * 4) / sqrt(4))
 print ("Five is", (4 / 4) + (sqrt(4) + sqrt(4)))
-print ("Six is", 4+4-4-4)
-print ("Seven is", 4+4-4-4)
+print ("Six is", 4 + sqrt(4) + (4 - 4))
+print ("Seven is", 4 + sqrt(4) + (4 / 4))
 print ("Eight is", sqrt(4) + sqrt(4) + sqrt(4) + sqrt(4))
-print ("Nine is", )
+print ("Nine is", (4 + 4) + (4 / 4))
 print ("Eleven is", 44 / (sqrt(4) + sqrt(4)))
-print ("Twelve is", sqrt(pow(4, 4)) - (sqrt(4) + sqrt(4)))
+print ("Twelve is", sqrt(4 ** 4) - (sqrt(4) + sqrt(4)))
 print ("Fourteen is", 4 * 4 - (4 - sqrt(4)))
+
+# I was stuck for a few minutes at 11 solutions then as soon as I thought of 6, I had seven
+# That is why I have 13 solution
