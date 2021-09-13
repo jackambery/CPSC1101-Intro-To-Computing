f4e569571835ff9b78698f6fbd1dc5371962ca12
Jack Ambery <jack.ambery@gmail.com>
2021-09-13 16:42:23 -0400

    Second Day classwork

---
+++ SecondDay.py
@@ -0,0 +1,14 @@
+#variables
+name = "rob the slob"
+age = 24
+
+print(name)
+print(age)
+
+# print with a string literal
+print("The value in the variable name:", name)
+print("The value in the variable age:", age)
+
+# input/output
+userNum = int(input("Please enter a number: "))
+print("The number you entered plus 10 is: ", userNum + 10)
