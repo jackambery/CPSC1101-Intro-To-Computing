f5ff7c1abb43e46300e269105125f800d557d576
Jack Ambery <jack.ambery@gmail.com>
2021-09-23 16:24:26 -0400

    started hw3

---
+++ homework/hw3.py
@@ -0,0 +1,27 @@
+# hw3
+# CS 101: Homework #3
+# Filename: hw3.py
+# Date: 23 Sept 2021
+#
+# Name: John (Jack) Ambery
+#
+# A program to 
+
+#q1a
+a = 1
+while a <= 12:
+    print(a)
+    a += 1
+
+#q1b
+for x in range(1,13):
+    print(x)
+
+#q2
+q2 = 1
+while q2 <= 12:
+    if (q2 % 2) == 1:
+        print(q2)
+    q2 += 1
+
+#q3
