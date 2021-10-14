5dd4d66713e8b165107d8f4a00aa54fd8df5c548
Jack Ambery <jack.ambery@gmail.com>
2021-10-14 16:34:52 -0400

    hw6 progress

---
+++ homework/hw6.py
@@ -0,0 +1,27 @@
+# hw6
+# CS 101: Homework #6
+# Filename: hw6.py
+# Date: 14 Oct 2021
+#
+# Name: John (Jack) Ambery
+#
+# A program to practice dictionaries and retrieving values from them
+
+vaca_supplies = ["sunscreen", "swimsuit", "hat", "shades"]
+
+stock = {
+    "sunscreen": 3,
+    "hat": 0,
+    "swimsuit": 2,
+    "shades": 15    
+}
+
+prices = {
+    "sunscreen": 4,
+    "hat": 2,
+    "swimsuit": 18.5,
+    "shades": 11
+}
+
+total = 0
+for item in vaca_supplies:
