80efeb608003298eafeae85ee359000d91c63e0e
Jack Ambery <jack.ambery@gmail.com>
2021-10-25 16:37:57 -0400

    hw7 added menu, cleaned up

--- homework/hw7.py
+++ homework/hw7.py
@@ -20,26 +20,31 @@ def withdrawl(bal, amount):
     result = bal - amount
     return result
 
+def menu():
+    print("Please type 'a', 'b', 'c', or 'd' for whichever option you desire.")
+    print("a) Print balance\nb) make deposit\nc) make withdrawl\nd) quit")
+
 # main method
 def main():
     balance = 500
     choice = input("Please type 'a' to start.\n")
     while (choice != 'd'):
-        print("Please type 'a', 'b', 'c', or 'd' for whichever option you desire.")
-        print("a) Print balance\nb) make deposit\nc) make withdrawl\nd) quit")
+        menu()
         if choice == 'a':
             printBal(balance)
-        if choice == 'b':
+        elif choice == 'b':
             amount = int(input("How much would you like to deposit?\n"))
             balance = deposit(balance, amount)
             printBal(balance)
-        if choice == 'c':
+        elif choice == 'c':
             amount = int(input("How much would you like to withdrawl?\n"))
             balance = withdrawl(balance, amount)
             printBal(balance)
+        else:
+            print("invalid selection")
         choice = input("What would you like to do?\n")
     # if d is selected
     print("Have a nice day!")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
